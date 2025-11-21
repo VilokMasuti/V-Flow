bb835b2f2f51c49096efca9a278094159cf041f7
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2025-11-21 12:44:36 +0530

    done with sidebarand mob-nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,20 @@
 import Navbar from "@/components/Nav/Index";
+import LeftNav from "@/components/Nav/SideBar/LeftNav";
+import RightNav from "@/components/Nav/SideBar/RightNav";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 realtive">
       <Navbar />
-      {children}
+      <div className="flex">
+        <LeftNav />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl bg-amber-200">{children}</div>
+        </section>
+
+        <RightNav />
+      </div>
     </main>
   );
 };
