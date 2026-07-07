5453afcdfd1f47e298dcfdd2c98a9073b4cd4384
Vilok <119694262+VilokMasuti@users.noreply.github.com>
2026-07-07 17:23:58 +0530

    feat: implement view transitions and adjust Lenis smooth scroll settings

--- components/lens/LenisProvider.jsx
+++ components/lens/LenisProvider.jsx
@@ -6,12 +6,12 @@ export function LenisProvider({ children }) {
     <ReactLenis
       root // Applies smooth scroll to the entire page
       options={{
-        lerp: 0.05, // Linear interpolation intensity (smoothness)
+        lerp: 0.08, // Linear interpolation intensity (smoothness)
         duration: 1.2, // Duration in seconds (only if lerp is not defined)
         smoothWheel: true, // Enables smooth scrolling for mouse wheel events
-        smoothTouch: true, // Enables smooth scrolling for touch events
+        smoothTouch: false, // Enables smooth scrolling for touch events
         wheelMultiplier: 1, // Multiplier for wheel scroll
-        touchMultiplier: 1, // Multiplier for touch scroll
+
         autoResize: true, // Automatically resize on window changes
         syncTouch: false, // Sync touch with scroll
       }}
